c22427192026793d0de19ea79a10c5f0b914c9e5
PrasannaShakya111222 <prasannashakya111@gmail.com>
2026-09-16 13:53:45 +0000

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let n = nums.length;
+        for (let i=0;i<n;i++){
+            if(nums[i]===target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
