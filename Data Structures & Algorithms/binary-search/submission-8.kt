a260511cc0d547ddea89dc3abdde78bb62789361
PrasannaShakya111222 <prasannashakya111@gmail.com>
2026-09-16 13:58:02 +0000

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        for (i in nums.indices) {
+            if (nums[i] == target) {
+                return i
+            }
+        }
+        return -1
+    }
+}
